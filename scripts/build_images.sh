3f3cf684292d375dc28297bf924bfa057a35bc54
sipeki <sipeki@kindlen.eu>
2020-07-28 10:32:25 +0000

    removed dot dot to dot for service image builds

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,20 +2,20 @@
 
 # Build service_1 iamge if does not exits localy
 if [[ "$(docker images -q sipeki/service_1:latest 2> /dev/null)" == "" ]]; then
-    docker build -t sipeki/service_1 ../Service_1
+    docker build -t sipeki/service_1 ./Service_1
 fi
 
 # Build service_2 iamge if does not exits localy
 if [[ "$(docker images -q sipeki/service_2:latest 2> /dev/null)" == "" ]]; then
-    docker build -t sipeki/service_2 ../Service_2
+    docker build -t sipeki/service_2 ./Service_2
 fi
 
 # Build service_3 iamge if does not exits localy
 if [[ "$(docker images -q sipeki/service_3:latest 2> /dev/null)" == "" ]]; then
-    docker build -t sipeki/service_3 ../Service_3
+    docker build -t sipeki/service_3 ./Service_3
 fi
 
 # Build service_4 iamge if does not exits localy
 if [[ "$(docker images -q sipeki/service_4:latest 2> /dev/null)" == " "]]; then
-    docker build -t sipeki/service_4 ../Service_4
+    docker build -t sipeki/service_4 ./Service_4
 fi
